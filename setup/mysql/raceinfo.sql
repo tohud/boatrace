ce045c3c7409ebfeb9df01dae34670e224834a95
tohud <ss16th@yahoo.co.jp>
2019-04-14 15:38:51 +0900

    mysql-raceinfo

---
+++ setup/mysql/raceinfo.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS raceinfo;
+
+CREATE TABLE raceinfo (
+    raceId CHAR(14),
+    raceDate DATE,
+    raceNumber TINYINT,
+    raceName VARCHAR(50),
+    raceGrade VARCHAR(5),
+    racePlaceId CHAR(2)
+);
+  PRIMARY KEY (raceId)
+  INDEX raceinfo_date_idx (raceDate)
+  
