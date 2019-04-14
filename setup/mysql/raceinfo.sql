d8b9c721cbbfb02ef8c9ea53c2cce1e1df5c0123
EC2 Default User <ec2-user@ip-172-31-45-253.us-east-2.compute.internal>
2019-04-14 06:44:42 +0000

    mysql

--- setup/mysql/raceinfo.sql
+++ setup/mysql/raceinfo.sql
@@ -6,8 +6,8 @@ CREATE TABLE raceinfo (
     raceNumber TINYINT,
     raceName VARCHAR(50),
     raceGrade VARCHAR(5),
-    racePlaceId CHAR(2)
+    racePlaceId CHAR(2),
+    PRIMARY KEY (raceId),
+    INDEX raceinfo_date_idx (raceDate)
 );
-  PRIMARY KEY (raceId)
-  INDEX raceinfo_date_idx (raceDate)
   
