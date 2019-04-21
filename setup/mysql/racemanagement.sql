6c423a9fae4c848480ad91ac089da8cec8556034
EC2 Default User <ec2-user@ip-172-31-45-253.us-east-2.compute.internal>
2019-04-21 08:11:55 +0000

    結果の取得中

--- setup/mysql/racemanagement.sql
+++ setup/mysql/racemanagement.sql
@@ -9,7 +9,12 @@ CREATE TABLE racemanagement (
     racePlaceId CHAR(2),
     racebeforeinfo_flg BOOLEAN,
     racemember_flg BOOLEAN,
-    raceodds_flg BOOLEAN,
+    raceodds3t_flg BOOLEAN,
+    raceodds3f_flg BOOLEAN,
+    raceodds2t_flg BOOLEAN,
+    raceodds2f_flg BOOLEAN,
+    raceodds1t_flg BOOLEAN,
+    raceodds1f_flg BOOLEAN,
     PRIMARY KEY (raceId),
     INDEX raceinfo_date_idx (raceDate)
 );
