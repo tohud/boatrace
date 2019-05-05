d431b527b6f51286c83b6b044cba360302cbbf32
EC2 Default User <ec2-user@ip-172-31-45-253.us-east-2.compute.internal>
2019-05-05 07:04:33 +0000

    不均衡データ問題対策中

--- learning/classify.py
+++ learning/classify.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[36]:
 
 
 # ToDo 選手情報・過去レース情報から3連単舟券120種をクラス分類する
@@ -17,7 +17,7 @@ import statsmodels.api as sm
 import tensorflow as tf
 
 
-# In[2]:
+# In[37]:
 
 
 # 自作ライブラリのimport
@@ -28,24 +28,24 @@ if os.environ['BR_HOME']+"/boatrace" not in sys.path:
 from setup.myUtil import dbHandler
 
 
-# In[3]:
+# In[38]:
 
 
 # 分析期間の指定は一旦ここでまとめてみる。
-trainStartDate="20180201"
-trainEndDate="20180228"
-testStartDate="20180301"
-testEndDate="20180331"
+trainStartDate="20180101"
+trainEndDate="20180531"
+testStartDate="20180401"
+testEndDate="20180430"
 
 
-# In[4]:
+# In[39]:
 
 
 dbh=dbHandler.getDBHandle()
 #dbHandler.closeDBHandle(dbh)
 
 
-# In[26]:
+# In[40]:
 
 
 # trainの元データを取得
@@ -63,46 +63,57 @@ with dbh.cursor() as cursor:
 print("testdatta:",len(testLoadList))
 
 
-# In[27]:
+# In[41]:
 
 
 df = pd.io.json.json_normalize(loadList)
 df.head()
 
 
-# In[28]:
+# In[42]:
 
 
 testdf = pd.io.json.json_normalize(testLoadList)
 testdf.head()
 
 
-# In[29]:
+# In[87]:
 
 
 # 入力のデータ整形
 xdf=df.drop(['funaken','odds','raceId','raceDate'],axis=1)
-xdf.head()
+xdf=pd.get_dummies(xdf,columns=['l1rank','l2rank','l3rank','l4rank','l5rank','l6rank'])
+#xdf.head()
+xdf.describe()
+
+# サンプル確認用
+xdf_sample=xdf[0:5]
 
 
-# In[30]:
+# In[44]:
 
 
 # 入力のデータ整形
 testxdf=testdf.drop(['funaken','odds','raceId','raceDate'],axis=1)
+testxdf=pd.get_dummies(testxdf,columns=['l1rank','l2rank','l3rank','l4rank','l5rank','l6rank'])
 testxdf.head()
 
 
-# In[8]:
+# In[130]:
 
 
 # 結果のOne-Hot表現を作る
 ydf=df['funaken']
 ydf=pd.get_dummies(ydf,columns=['funaken'])
-ydf.head()
+#ydf.head()
+ydf.describe()
+
+# サンプル確認用
+#ydf_sample=ydf[0:5]
+#ydf.head()
 
 
-# In[31]:
+# In[46]:
 
 
 # 結果のOne-Hot表現を作る
@@ -111,56 +122,142 @@ testydf=pd.get_dummies(testydf,columns=['funaken'])
 testydf.head()
 
 
-# In[15]:
+# In[47]:
 
 
-xAtrNum=38
-yclassNum=120
+#xAtrNum=62
+#yclassNum=120
+
 # create the model
+## 保存しておく
+#x = tf.placeholder(tf.float32,[None,xAtrNum])
+#W = tf.Variable(tf.random_uniform([xAtrNum,yclassNum],-1.0,1.0))
+#b = tf.Variable(tf.random_uniform([yclassNum],-1.0,1.0))
+#y = tf.nn.softmax( (tf.matmul(x,W)+b) - tf.reduce_max(tf.matmul(x,W)+b)  )
+
+
+# In[123]:
+
+
+xAtrNum=62
+yclassNum=120
+
+# create the model
+## 保存しておく
+#x = tf.placeholder(tf.float32,[None,xAtrNum])
+#W = tf.Variable(tf.random_uniform([xAtrNum,yclassNum],-1.0,1.0))
+#b = tf.Variable(tf.random_uniform([yclassNum],-1.0,1.0))
+#y = tf.nn.softmax( (tf.matmul(x,W)+b) - tf.reduce_max(tf.matmul(x,W)+b)  )
+
+# layerNumは2以上。
+layerNum=5
+layerClassNum=360
+wInitMin= -0.1
+wInitMax= 0.1
+bInitMin= -0.1
+bInitMax= 0.1
+
+## 多層化
 x = tf.placeholder(tf.float32,[None,xAtrNum])
-#W = tf.Variable(tf.zeros([xAtrNum,yclassNum]))
-#b = tf.Variable(tf.zeros([yclassNum]))
-W = tf.Variable(tf.random_uniform([xAtrNum,yclassNum],-1.0,1.0))
-b = tf.Variable(tf.random_uniform([yclassNum],-1.0,1.0))
-y = tf.nn.softmax( (tf.matmul(x,W)+b) - tf.reduce_max(tf.matmul(x,W)+b)  )
+x_hidden=[]
+W_hidden=[]
+b_hidden=[]
+
+# 0 と最後については形が違うので個別に設定。
+x_hidden.append(tf.Variable(tf.random_uniform([len(loadList),layerClassNum],-1.0,1.0)))
+W_hidden.append(tf.Variable(tf.random_uniform([xAtrNum,layerClassNum],wInitMin,wInitMax)))
+b_hidden.append(tf.Variable(tf.random_uniform([layerClassNum],bInitMin,bInitMax)))
+
+for i in range(1,layerNum):
+    x_hidden.append(tf.Variable(tf.random_uniform([len(loadList),layerClassNum],-1.0,1.0)))
+    W_hidden.append(tf.Variable(tf.random_uniform([layerClassNum,layerClassNum],wInitMin,wInitMax)))
+    b_hidden.append(tf.Variable(tf.random_uniform([layerClassNum],bInitMin,bInitMax)))
+
+x_hidden[0]=tf.sigmoid(tf.matmul(x,W_hidden[0])+b_hidden[0])
+for i in range(1,layerNum):
+    x_hidden[i]=tf.sigmoid(tf.matmul(x_hidden[i-1],W_hidden[i])+b_hidden[i])
+
+# 0 と最後については形が違うので個別に設定。
+W = tf.Variable(tf.random_uniform([layerClassNum,yclassNum],wInitMin,wInitMax))
+b = tf.Variable(tf.random_uniform([yclassNum],bInitMin,bInitMax))
+
+y = tf.nn.softmax( (tf.matmul(x_hidden[layerNum-1],W)+b) - tf.reduce_max(tf.matmul(x_hidden[layerNum-1],W)+b)  )
+#y = tf.nn.softmax( tf.sigmoid(tf.matmul(x_hidden[layerNum-1],W)+b))
 
 
-# In[20]:
+# In[124]:
 
 
 # define loss and optimizer
 y_ = tf.placeholder(tf.float32,[None,yclassNum])
-cross_entropy=-tf.reduce_sum(y_*tf.log( tf.clip_by_value(y,1e-10,1e+10) ))
+cross_entropy=-tf.reduce_sum(y_*tf.log( tf.clip_by_value(y,1e-5,1e+5) ))
+
+# L2正則化
+lambda_2=0.01
+L2_sqr=tf.nn.l2_loss(W)
+for i in range(layerNum):
+    L2_sqr+=tf.nn.l2_loss(W_hidden[i])
+
+loss=cross_entropy+lambda_2*L2_sqr
+
 #cross_entropy=-tf.reduce_sum( (y_-y)**2 )
-train_step=tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+train_step=tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+sess = tf.Session()
 
 
-# In[25]:
+# In[125]:
+
+
+# tensorboard
+with tf.name_scope('summary'):
+    tf.summary.scalar('cross_entropy', cross_entropy)
+    merged = tf.summary.merge_all()
+    writer = tf.summary.FileWriter('/home/ec2-user/boatrace/log/tensorboard', sess.graph)
+
+
+# In[126]:
 
 
 # train
-init = tf.initialize_all_variables()
-sess = tf.Session()
+maxEpochs=1000
+
+init = tf.global_variables_initializer()
 sess.run(init)
-for i in range(1000):
+for i in range(maxEpochs):
     sess.run(train_step,feed_dict={x:xdf.values,y_:ydf.values})
-    if i % 100 == 0:
-        print("cross_entropy:"+str(sess.run(cross_entropy,feed_dict={x:xdf.values,y_:ydf.values}) ))
+    if i % (maxEpochs/10) == 0:
         correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        print("accuracy:"+str(sess.run(accuracy, feed_dict={x: xdf.values, y_: ydf.values})))
+        print("cross_entropy:"+str(sess.run(cross_entropy,feed_dict={x:xdf.values,y_:ydf.values}) ),               "loss:"+str(sess.run(loss,feed_dict={x:xdf.values,y_:ydf.values})),                "accuracy:"+str(sess.run(accuracy, feed_dict={x: xdf.values, y_: ydf.values})) )
+        #print("w:",sess.run(tf.reduce_sum(W),feed_dict={x: xdf.values, y_: ydf.values}))
+        print("Ymax:",sess.run(tf.reduce_max(y) ,feed_dict={x:xdf.values,y_:ydf.values}))
+        print("Ymin:",sess.run(tf.reduce_min(y) ,feed_dict={x:xdf.values,y_:ydf.values}))
+        print("W:",sess.run(tf.reduce_sum(W) ,feed_dict={x:xdf.values,y_:ydf.values}))
+        for j in range(layerNum):
+            print("X%d:" % (j),sess.run(tf.reduce_max(x_hidden[j]) ,feed_dict={x:xdf.values,y_:ydf.values}))
+        #print("b:",sess.run(tf.reduce_sum(b) ,feed_dict={x:xdf.values,y_:ydf.values}))
+print("sample:",sess.run(x_hidden[0],feed_dict={x:xdf_sample.values,y_:ydf_sample.values}))
+        
 
 
-# In[32]:
+# In[128]:
 
 
-# test_trained_data
+print("sample:",sess.run(x_hidden[layerNum-1],feed_dict={x:xdf_sample.values,y_:ydf_sample.values}))
+print("Y:",sess.run(y,feed_dict={x:xdf_sample.values,y_:ydf_sample.values}))
+
+
+# In[80]:
+
+
+# evaluate trained_data
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 print(sess.run(accuracy, feed_dict={x: testxdf.values, y_: testydf.values}))
 
 
-# In[33]:
+# In[53]:
 
 
 # メモリ使用チェック
--- setup/mysql/raceabst_forml_v.sql
+++ setup/mysql/raceabst_forml_v.sql
@@ -1,12 +1,12 @@
 CREATE OR REPLACE VIEW raceabst_forml_v
 as
 select g.raceId,g.raceDate,
-       m1.Fcnt as l1Fcnt,m1.motor2r as l1motor2r,m1.motor3r as l1motor3r,m1.boat2r as l1boat2r,m1.boat3r as l1boat3r,1000/o1.avgo as l1score,
-       m2.Fcnt as l2Fcnt,m2.motor2r as l2motor2r,m2.motor3r as l2motor3r,m2.boat2r as l2boat2r,m2.boat3r as l2boat3r,1000/o2.avgo as l2score,
-       m3.Fcnt as l3Fcnt,m3.motor2r as l3motor2r,m3.motor3r as l3motor3r,m3.boat2r as l3boat2r,m3.boat3r as l3boat3r,1000/o3.avgo as l3score,
-       m4.Fcnt as l4Fcnt,m4.motor2r as l4motor2r,m4.motor3r as l4motor3r,m4.boat2r as l4boat2r,m4.boat3r as l4boat3r,1000/o4.avgo as l4score,
-       m5.Fcnt as l5Fcnt,m5.motor2r as l5motor2r,m5.motor3r as l5motor3r,m5.boat2r as l5boat2r,m5.boat3r as l5boat3r,1000/o5.avgo as l5score,
-       m6.Fcnt as l6Fcnt,m6.motor2r as l6motor2r,m6.motor3r as l6motor3r,m6.boat2r as l6boat2r,m6.boat3r as l6boat3r,1000/o6.avgo as l6score,
+       m1.rmRank as l1rank, m1.Fcnt as l1Fcnt,m1.motor2r/100.0 as l1motor2r,m1.motor3r/100.0 as l1motor3r,m1.boat2r/100.0 as l1boat2r,m1.boat3r/100.0 as l1boat3r,1000/o1.avgo as l1score,
+       m2.rmRank as l2rank, m2.Fcnt as l2Fcnt,m2.motor2r/100.0 as l2motor2r,m2.motor3r/100.0 as l2motor3r,m2.boat2r/100.0 as l2boat2r,m2.boat3r/100.0 as l2boat3r,1000/o2.avgo as l2score,
+       m3.rmRank as l3rank, m3.Fcnt as l3Fcnt,m3.motor2r/100.0 as l3motor2r,m3.motor3r/100.0 as l3motor3r,m3.boat2r/100.0 as l3boat2r,m3.boat3r/100.0 as l3boat3r,1000/o3.avgo as l3score,
+       m4.rmRank as l4rank, m4.Fcnt as l4Fcnt,m4.motor2r/100.0 as l4motor2r,m4.motor3r/100.0 as l4motor3r,m4.boat2r/100.0 as l4boat2r,m4.boat3r/100.0 as l4boat3r,1000/o4.avgo as l4score,
+       m5.rmRank as l5rank, m5.Fcnt as l5Fcnt,m5.motor2r/100.0 as l5motor2r,m5.motor3r/100.0 as l5motor3r,m5.boat2r/100.0 as l5boat2r,m5.boat3r/100.0 as l5boat3r,1000/o5.avgo as l5score,
+       m6.rmRank as l6rank, m6.Fcnt as l6Fcnt,m6.motor2r/100.0 as l6motor2r,m6.motor3r/100.0 as l6motor3r,m6.boat2r/100.0 as l6boat2r,m6.boat3r/100.0 as l6boat3r,1000/o6.avgo as l6score,
        b.raceWindSpeed,b.raceWaveHeight,
        s.funaken,s.odds
 from 
